0062d801847834a639e085f9c1ea1061c24ce70e
Rob Mellett <robmellett@gmail.com>
2020-02-09 21:05:09 +0700

    Add product page

--- pages/products/_handle.vue
+++ pages/products/_handle.vue
@@ -1,33 +1,28 @@
 <template>
   <div>
-
+    <h1>{{ product.title }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: '_slug',
+  name: 'ProductPage',
 
-  props: {
+  async asyncData({ $shopify, params }) {
+    let product = await $shopify.product.fetchByHandle(params.handle)
+      .then((product) => {
+        if (!product) {
+          error({ statusCode: 403, message: 'Product does not have Storefront Permissions' })
+        }
+        return product;
+      })
+      .catch((err) => {
+        error({ statusCode: 404, message: 'Product not found' })
+      });
 
-  },
-
-  components: {
-
-  },
-
-  data() {
     return {
-
+      product: product,
     }
-  },
-
-  computed: {
-
-  },
-
-  methods: {
-
-  },
+  }
 }
 </script>
