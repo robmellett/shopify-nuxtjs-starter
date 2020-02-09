f26e8387926554189e3628baf3cb3c688c7abed8
Rob Mellett <robmellett@gmail.com>
2020-02-09 20:57:03 +0700

    Add product handle route

---
+++ pages/products/_handle.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: '_slug',
+
+  props: {
+
+  },
+
+  components: {
+
+  },
+
+  data() {
+    return {
+
+    }
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+
+  },
+}
+</script>
