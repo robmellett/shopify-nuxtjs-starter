0b14a7e645e3ec66acebe0f75dac7f3bbe19f711
Rob Mellett <robmellett@gmail.com>
2020-02-09 21:31:30 +0700

    Add page

---
+++ pages/pages/_handle.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ page.title }}</h1>
+    <div class="content" v-html="page.body">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Page',
+
+  async asyncData({ $shopify, params, error }) {
+    const query = await $shopify.graphQLClient.query((root) => {
+      root.add('pageByHandle', { args: { handle: params.handle } }, (page) => {
+        page.add('title');
+        page.add('body');
+      });
+    });
+
+    let page = await $shopify.graphQLClient.send(query).then(({ data }) => {
+      if (!data.pageByHandle) {
+        error({ statusCode: 404, message: 'Page not found' })
+      }
+
+      return data.pageByHandle;
+    });
+
+    return {
+      page: page,
+    }
+  }
+}
+</script>
