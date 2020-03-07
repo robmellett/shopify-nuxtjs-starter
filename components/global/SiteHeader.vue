8f79fca3de1c57e26a793a4e8a4605eeaf176f81
Rob <dev@robmellett.com>
2020-03-07 17:41:31 +1000

    Add light logo

--- components/global/SiteHeader.vue
+++ components/global/SiteHeader.vue
@@ -6,13 +6,13 @@
           <div class="flex-shrink-0">
             <img
               class="block lg:hidden h-8 w-auto"
-              src="/img/logos/workflow-mark-on-dark.svg"
-              alt=""
+              src="~assets/images/acme-logo-light.svg"
+              alt="Site Logo"
             />
             <img
               class="hidden lg:block h-8 w-auto"
-              src="/img/logos/workflow-logo-on-dark.svg"
-              alt=""
+              src="~assets/images/acme-logo-light.svg"
+              alt="Site Logo"
             />
           </div>
           <div class="hidden sm:block sm:ml-6">
@@ -50,21 +50,7 @@
         </div>
         <div class="hidden sm:ml-6 sm:block">
           <div class="flex items-center">
-            <button class="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">
-              <svg
-                class="h-6 w-6"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
-            </button>
+            <cart-icon />
           </div>
         </div>
         <site-navigation-button />
@@ -75,13 +61,15 @@
 </template>
 
 <script>
-const  SiteNavigationButton = () => import("~/components/navigation/SiteNavigationButton");
-const  SiteNavigationDropdown = () => import("~/components/navigation/SiteNavigationDropDown");
+const CartIcon = () => import("~/components/cart/CartIcon");
+const SiteNavigationButton = () => import("~/components/navigation/SiteNavigationButton");
+const SiteNavigationDropdown = () => import("~/components/navigation/SiteNavigationDropDown");
 
 export default {
   name: 'SiteHeader',
 
   components: {
+    CartIcon,
     SiteNavigationButton,
     SiteNavigationDropdown
   },
@@ -94,10 +82,9 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .nuxt-link-exact-active {
-  @apply
-    px-3
+  @apply px-3
     py-2
     rounded-md
     text-sm
@@ -107,10 +94,9 @@ export default {
     bg-gray-900;
 
   &:focus {
-    @apply
-      outline-none
+    @apply outline-none
       text-white
-      bg-gray-700
+      bg-gray-700;
   }
 }
 </style>
