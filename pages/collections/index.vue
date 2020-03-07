240f410473f105e32869c6807b1b4b13159a98b2
Rob <dev@robmellett.com>
2020-03-07 15:19:03 +1000

    Add site pages

---
+++ pages/collections/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    All the collections
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+
+  props: {
+
+  },
+
+  components: {
+
+  },
+
+  data() {
+    return {
+
+    }
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+
+  },
+}
+</script>
---
+++ pages/products/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    All the products
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+
+  props: {
+
+  },
+
+  components: {
+
+  },
+
+  data() {
+    return {
+
+    }
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+
+  },
+}
+</script>
