6cfd13cbaddaf77403fb40e767d47fe6e303120e
Rob <dev@robmellett.com>
2020-03-07 15:42:44 +1000

    Header

--- components/global/SiteHeader.vue
+++ components/global/SiteHeader.vue
@@ -1,5 +1,169 @@
 <template>
-
+  <nav class="bg-gray-800">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex items-center justify-between h-16">
+        <div class="flex items-center">
+          <div class="flex-shrink-0">
+            <img
+              class="block lg:hidden h-8 w-auto"
+              src="/img/logos/workflow-mark-on-dark.svg"
+              alt=""
+            />
+            <img
+              class="hidden lg:block h-8 w-auto"
+              src="/img/logos/workflow-logo-on-dark.svg"
+              alt=""
+            />
+          </div>
+          <div class="hidden sm:block sm:ml-6">
+            <div class="flex">
+              <nuxt-link
+                :to="{ name: 'index' }"
+                class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+                exact
+              >
+                Home
+              </nuxt-link>
+              <nuxt-link
+                :to="{ name: 'products' }"
+                class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+                exact
+              >
+                Products
+              </nuxt-link>
+              <nuxt-link
+                :to="{ name: 'collections' }"
+                class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+                exact
+              >
+                Collections
+              </nuxt-link>
+              <nuxt-link
+                :to="{ name: 'articles' }"
+                class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+                exact
+              >
+                Blog
+              </nuxt-link>
+            </div>
+          </div>
+        </div>
+        <div class="hidden sm:ml-6 sm:block">
+          <div class="flex items-center">
+            <button class="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">
+              <svg
+                class="h-6 w-6"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div class="-mr-2 flex sm:hidden">
+          <button
+            @click="open = !open"
+            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out"
+          >
+            <svg
+              class="h-6 w-6"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                :class="{'hidden': open, 'inline-flex': !open }"
+                class="inline-flex"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+              <path
+                :class="{'hidden': !open, 'inline-flex': open }"
+                class=""
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div
+      :class="{'block': open, 'hidden': !open}"
+      class="sm:hidden"
+    >
+      <div class="px-2 pt-2 pb-3">
+        <nuxt-link
+          :to="{ name: 'index' }"
+          class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+          exact
+        >
+          Home
+        </nuxt-link>
+        <nuxt-link
+          :to="{ name: 'products' }"
+          class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+          exact
+        >
+          Products
+        </nuxt-link>
+        <nuxt-link
+          :to="{ name: 'collections' }"
+          class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+          exact
+        >
+          Collections
+        </nuxt-link>
+        <nuxt-link
+          :to="{ name: 'articles' }"
+          class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+          exact
+        >
+          Blog
+        </nuxt-link>
+      </div>
+      <div class="pt-4 pb-3 border-t border-gray-700">
+        <div class="flex items-center px-5">
+          <div class="flex-shrink-0">
+            <img
+              class="h-10 w-10 rounded-full"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+          </div>
+          <div class="ml-3">
+            <div class="text-base font-medium leading-6 text-white">Tom Cook</div>
+            <div class="text-sm font-medium leading-5 text-gray-400">tom@example.com</div>
+          </div>
+        </div>
+        <div class="mt-3 px-2">
+          <a
+            href="#"
+            class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+          >Your Profile</a>
+          <a
+            href="#"
+            class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+          >Settings</a>
+          <a
+            href="#"
+            class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out"
+          >Sign out</a>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -13,3 +177,21 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.nuxt-link-exact-active {
+  @apply
+    px-3
+    py-2
+    rounded-md
+    text-sm
+    font-medium
+    leading-5
+    text-white
+    bg-gray-900;
+
+  &:focus {
+    @apply outline-none text-white bg-gray-700
+  }
+}
+</style>
