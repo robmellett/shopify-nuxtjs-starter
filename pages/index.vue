929c666f93af973150d19ba38bd3710d6a45ed7d
Rob <dev@robmellett.com>
2020-03-07 19:45:05 +1000

    Tidy up

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="container">
-    <h1>Shopify Store Demo</h1>
+    <h1 class="mt-8 text-center text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
+      Shopify Store Demo
+    </h1>
 
-    <ul>
+    <ul class="mt-4 text-center">
       <li v-for="product in products" :key="product.id">
         <nuxt-link :to="{ name: 'products-handle', params: { 'handle' : product.handle }}">
           {{ product.title }}
         </nuxt-link>
       </li>
     </ul>
-
   </div>
 </template>
 
