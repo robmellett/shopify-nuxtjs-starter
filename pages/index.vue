d9d58c1baca7f71135beb20ba45b8937d8d2b704
Rob <dev@robmellett.com>
2020-03-08 12:50:57 +1000

    Center the container

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container mx-auto">
     <h1 class="mt-8 text-center text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
       Shopify Store Demo
     </h1>
